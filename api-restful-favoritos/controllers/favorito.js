4402edf6dbc38098f49a33b9a61fdd8bf4842918
Beatriz del Pino García <bpingar@gmail.com>
2017-02-09 19:13:36 +0100

    Finalizada sección 2.15

---
+++ api-restful-favoritos/controllers/favorito.js
@@ -0,0 +1,50 @@
+'use strict'
+
+function prueba(req, res) {
+
+	if(req.params.nombre){
+		var nombre=req.params.nombre;
+	}else{
+		var nombre = "SIN NOMBRE";		
+	}
+	
+	res.status(200).send({
+						data: [2,3,4],
+						texto: "Hola mundo con NodeJS y EXPRESS - " + nombre
+					});
+}
+
+function getFavorito(req, res){
+	var favoritoId = req.params.id;
+	res.status(200).send({data:favoritoId});
+}
+
+function getFavoritos(req, res){
+	
+}
+
+function saveFavorito(req, res){
+	var params = req.body;
+	res.status(200).send({favorito: params});
+}
+
+function updateFavorito(req, res){
+	var params = req.body;
+	res.status(200).send({update:true, favorito: params});
+	
+}
+
+function deleteFavorito(req, res){
+	var favoritoId = req.params.id;
+	res.status(200).send({delete:true, data: favoritoId});
+	
+}
+
+module.exports = {
+    prueba,
+	getFavorito,
+	getFavoritos,
+	saveFavorito,
+	updateFavorito,
+	deleteFavorito
+} 
--- api-restful-favoritos/index.js
+++ api-restful-favoritos/index.js
@@ -1,31 +1,19 @@
 'use strict'
 
-var express = require('express');
-var bodyParser = require('body-parser');
 
-var app = express();
+var mongoose = require('mongoose');
+var app = require('./app');
 var port = process.env.PORT || 3678;
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-
-							//function (req, res)
-app.get('/prueba/:nombre?', (req, res) =>{
-
-	if(req.params.nombre){
-		var nombre=req.params.nombre;
+mongoose.connect('mongodb://localhost:27017/cursofavoritos',(err, res)=>{
+	if(err){
+		throw err;
 	}else{
-		var nombre = "SIN NOMBRE";		
-	}
-	
-	res.status(200).send({
-						data: [2,3,4],
-						texto: "Hola mundo con NodeJS y EXPRESS - " + nombre
-					});
+		console.log('Conexión a MongoDB correcta');
+		app.listen(port,function(){
+			console.log(`API REST FAVORITOS funcionando en http://localhost:${port}`);
+		});
+	}	
 });
 
-				
-app.listen(port,function(){
-	console.log(`API REST FAVORITOS funcionando en http://localhost:${port}`);
-});
-
+ 
