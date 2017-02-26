4aa5f13b922464553cb98329d9e51a0d860c2e2b
Beatriz del Pino García <bpingar@gmail.com>
2017-02-26 12:05:46 +0100

    Finalizada sección 3.28

--- client-favoritos/app/app.component.js
+++ client-favoritos/app/app.component.js
@@ -5,20 +5,25 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 // Importar Component desde el núcleo de Angular
 var core_1 = require("@angular/core");
 // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
 var AppComponent = (function () {
-    // Clase del componente donde irán los datos y funcionalidades
     function AppComponent() {
+        this.title = 'APP FAVORITOS TITULO';
+        this.description = 'Aplicación web SPA con Angular 2 para gestionar favoritos online';
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: '<h1>Hola mundo con Angular 2 !! victorrobleswebbea.es</h1>'
-    })
+        templateUrl: 'app/views/home.html'
+    }),
+    __metadata("design:paramtypes", [])
 ], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
