59c265cd68ca8c04a60b1b95fb955c463af58c33
Beatriz del Pino García <bpingar@gmail.com>
2017-03-05 14:43:01 +0100

    Finalizada sección 4.34

--- client-favoritos/app/app.module.ts
+++ client-favoritos/app/app.module.ts
@@ -1,17 +1,22 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
 import { FavoritosListComponent }  from './components/favoritos-list.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports: [ 
+            BrowserModule, 
+            FormsModule,
+            HttpModule
+  ],
   declarations: [ 
                   AppComponent,
                   FavoritosListComponent
-                ],
-  bootstrap:    [ AppComponent ]
+  ],
+  bootstrap: [ AppComponent ]
 })
  
 export class AppModule { }
---
+++ client-favoritos/app/models/favorito.ts
@@ -0,0 +1,9 @@
+export class Favorito{
+    constructor(
+        public title: string,
+        public description: string,
+        public url: string
+    ){
+
+    }
+}
