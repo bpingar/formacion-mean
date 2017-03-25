b6ec2a888d36bd6276108c8d9b3c8ad476e4a5aa
Beatriz del Pino García <bpingar@gmail.com>
2017-03-25 19:12:41 +0100

    Finalizada sección 4.38

--- client-favoritos/app/components/favorito-detail.component.ts
+++ client-favoritos/app/components/favorito-detail.component.ts
@@ -1,17 +1,51 @@
-// Importar Component desde el núcleo de Angular
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router, ActivatedRoute, Params} from '@angular/router'; 
 
-// Decorador component, indicamos en que etiqueta se va a cargar la plantilla
+import {FavoritoService} from '../services/favorito.service';
+import {Favorito} from '../models/favorito';
+
 @Component({
     selector: 'favorito-detail',
-    templateUrl: 'app/views/favorito-detail.html'
+    templateUrl: 'app/views/favorito-detail.html',
+    providers: [FavoritoService]
 })
  
-// Clase del componente donde irán los datos y funcionalidades
-export class FavoritoDetailComponent { 
+export class FavoritoDetailComponent implements OnInit { 
+    public favorito: Favorito;
+    public errorMessage;
 
-    constructor(){
+    constructor(
+        private _favoritoService: FavoritoService,
+        private _route: ActivatedRoute,
+        private _router: Router
+    ){}
+
+    ngOnInit(){
+        this.getFavorito();
+    }
+
+    getFavorito(){
+        this._route.params.forEach((params: Params)=>{
+            let id =params['id'];
+
+            this._favoritoService.getFavorito(id).subscribe(
+                response=>{
+                    this.favorito=response.favorito;
+
+                    if(!this.favorito){
+                        this._router.navigate(['/']);
+                    }
+                },
+                error=>{
+                    this.errorMessage = <any>error;
+                
+                    if(this.errorMessage!=null){
+                        console.log(this.errorMessage);
+                        alert('Error en la petición');
+                    }                    
+                }
+            );
+        });
     }
 
 }
